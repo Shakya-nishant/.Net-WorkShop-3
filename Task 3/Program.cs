c03e3400f21bcffb1d5f7805c89207a512271af5
Shakya-nishant <155061583+Nishant10011@users.noreply.github.com>
2025-11-10 11:57:41 +0545

    Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter Your Age");
+int Age = Convert.ToInt32(Console.ReadLine());
+if (Age < 13)
+{
+    Console.WriteLine("Child");
+}
+else if (Age >= 13 || Age <= 19)
+{
+    Console.WriteLine("TeenAge");
+}
+else
+{
+    Console.WriteLine("Adult");
+}
