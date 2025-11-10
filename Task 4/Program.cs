b233737ce536605f29c8ae93aa371d9f47bd63bf
Shakya-nishant <155061583+Nishant10011@users.noreply.github.com>
2025-11-10 12:04:59 +0545

    Task 3

---
+++ Task 4/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Enter a number (1-7) to get the day of the week: ");
+
+int day;
+int.TryParse(Console.ReadLine(), out day);
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Sunday");
+        break;
+    case 2:
+        Console.WriteLine("Monday");
+        break;
+    case 3:
+        Console.WriteLine("Tuesday");
+        break;
+    case 4:
+        Console.WriteLine("Wednesday");
+        break;
+    case 5:
+        Console.WriteLine("Thursday");
+        break;
+    case 6:
+        Console.WriteLine("Friday");
+        break;
+    case 7:
+        Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.WriteLine("Invalid input! Please enter a number between 1 and 7.");
+        break;
+}
