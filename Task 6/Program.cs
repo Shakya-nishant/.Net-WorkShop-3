101592ee22bd9f98282a425799280e700896bce1
Shakya-nishant <155061583+Nishant10011@users.noreply.github.com>
2025-11-10 12:26:46 +0545

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,35 @@
+﻿
+try
+{
+    Console.Write("Enter a number: ");
+    string input = Console.ReadLine();
+    int number = Convert.ToInt32(input);
+    Console.WriteLine("You entered: " + number);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid number format");
+}
+finally
+{
+    Console.WriteLine("Program Executed");
+}
+Console.Write("\nEnter a password: ");
+string password = Console.ReadLine();
+
+try
+{
+    if (password.Length < 6)
+    {
+        throw new Exception("Password must be at least 6 characters.");
+    }
+    else
+    {
+        Console.WriteLine("Password satisfied the requirement");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
